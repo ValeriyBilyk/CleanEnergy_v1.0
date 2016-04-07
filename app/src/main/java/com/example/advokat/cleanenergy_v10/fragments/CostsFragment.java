1ce1b2d92ca1746177d7597cec777059372caed7
ValeriyBilyk <bilyk1996@gmail.com>
2016-04-07 15:26:13 +0300

    design

--- app/src/main/java/com/example/advokat/cleanenergy_v10/fragments/CostsFragment.java
+++ app/src/main/java/com/example/advokat/cleanenergy_v10/fragments/CostsFragment.java
@@ -2,12 +2,19 @@ package com.example.advokat.cleanenergy_v10.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.advokat.cleanenergy_v10.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CostsFragment extends Fragment {
 
@@ -15,6 +22,50 @@ public class CostsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.include_viewpager, container, false);
+        ViewPager viewPager = (ViewPager) v.findViewById(R.id.viewpager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
+
+        TabLayout tabLayout = (TabLayout) v.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
         return v;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "Постійні");
+        adapter.addFragment(new TwoFragment(), "Непостійні");
+        viewPager.setAdapter(adapter);
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
     }
 }
